7d750547544f79ab1045126419ac68a7c6be0efa
Shadow1452 <99354588+Shadow1452@users.noreply.github.com>
2023-05-27 11:36:44 +0200

    Update colornmap.py

--- colorfullnmap.py
+++ colorfullnmap.py
@@ -2,13 +2,21 @@ import argparse
 import nmap
 import colorama
 from colorama import Fore, Style
+from tqdm import tqdm
 
 # Initialize colorama
 colorama.init(autoreset=True)
 
-def run_nmap_scan(target, ports):
+def run_nmap_scan(target, ports, disable_ping, show_version):
     scanner = nmap.PortScanner()
-    scanner.scan(target, arguments='-p {}'.format(ports))
+    arguments = '-p {} {} {}'.format(ports, '-Pn' if disable_ping else '', '-sV' if show_version else '')  # Add -sV to show version if enabled
+    scanner.scan(target, arguments=arguments)
+
+    print(Fore.YELLOW + "Scanning target: {}".format(target))
+    print()
+
+    total_ports = sum(len(scanner[host][proto]) for host in scanner.all_hosts() for proto in scanner[host])
+    progress_bar = tqdm(total=total_ports, unit='port', ncols=80)
 
     for host in scanner.all_hosts():
         print(Fore.GREEN + f"Host: {host} ({scanner[host].hostname()})")
@@ -21,13 +29,36 @@ def run_nmap_scan(target, ports):
             for port in ports:
                 state = scanner[host][proto][port]['state']
                 service = scanner[host][proto][port]['name']
-                print(f"Port: {port}\tState: {state}\tService: {service}")
+                if show_version:
+                    version = scanner[host][proto][port]['version']
+                    if version:
+                        print(Fore.WHITE + f"  Port: {port}")
+                        print(Fore.WHITE + "    State: {}".format(state))
+                        print(Fore.WHITE + "    Service: {}".format(service))
+                        print(Fore.WHITE + "    Version: {}".format(version))
+                        print()
+                    else:
+                        print(Fore.WHITE + f"  Port: {port}")
+                        print(Fore.WHITE + "    State: {}".format(state))
+                        print(Fore.WHITE + "    Service: {}".format(service))
+                        print()
+                else:
+                    print(Fore.WHITE + f"  Port: {port}")
+                    print(Fore.WHITE + "    State: {}".format(state))
+                    print(Fore.WHITE + "    Service: {}".format(service))
+                    print()
 
+                progress_bar.update(1)
+    progress_bar.close()
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Colorful Nmap Scanner')
+    parser = argparse.ArgumentParser(description='Colorful Nmap Scanner', formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('target', help='Target IP address or hostname')
     parser.add_argument('-p', '--ports', default='1-1000', help='Port range to scan (default: 1-1000)')
+    parser.add_argument('-Pn', dest='disable_ping', action='store_true', help='Block ping probes')
+    parser.add_argument('-sV', dest='show_version', action='store_true', help='Show version of found ports')
+    parser.add_argument('--version', action='version', version='%(prog)s 2.0.0', help='Show the version number and exit')
+    parser.add_argument('-h', '--help', action='help', help='Show this help message and exit')
     args = parser.parse_args()
 
-    run_nmap_scan(args.target, args.ports)
+    run_nmap_scan(args.target, args.ports, args.disable_ping, args.show_version)
